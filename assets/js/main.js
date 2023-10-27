350f4b040521a5cf986aeab52edfe237df56d4da
Yassine Baghdadi <yassine.baghdadi.deve@gmail.com>
2023-10-27 17:28:29 +0100

    add new images

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,5 +30,27 @@
 			speed: 1000,
 			offset: function() { return $nav.height(); }
 		});
+		
+
+
+		const imageElement = document.getElementById('thePic');
+		const imageUrls = ['images/me/pic00.jpg', 'images/me/pic01.jpg', 'images/me/p02.jpg', 'images/me/p03.jpg', 'images/me/p04.jpg', 
+		'images/me/p05.jpg', 'images/me/p06.jpg', 'images/me/p07.jpg', 'images/me/p08.jpg', 'images/me/p09.jpg'];
+		let currentIndex = 0;
+
+		function changeImage() {
+		imageElement.classList.add('flip');
+		setTimeout(() => {
+			currentIndex = (currentIndex + 1) % imageUrls.length;
+			imageElement.src = imageUrls[currentIndex];
+			imageElement.classList.remove('flip');
+		}, 2000); // 1 second for the flip animation
+		}
+
+		// Initial image change
+		// changeImage();
+
+		// Set an interval to change images every 2 seconds
+		setInterval(changeImage, 2000);
 
 })(jQuery);
