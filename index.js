5dc0efcf8e6a2239daf36ec102e23f2c7cf7b2f2
Yassine Baghdadi <yassine.baghdadi.deve@gmail.com>
2025-09-24 11:13:44 +0100

    update

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const fs = require('fs');
 app.use(cookieParser());
 app.set('trust proxy', true);
 
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
